b846d3e78170c166c5b3c456ce24d6e455ec70e9
Rolando Carralero <rolandocape@gmail.com>
2022-07-05 21:32:07 -0400

    first commit

---
+++ src/challenge-1-vanilla.ts
@@ -0,0 +1,5 @@
+// Note: The HTML for this challenge can be found in index.html
+// Note: this function is run inside of src/main.tsx
+export function runVanillaApp() {
+  // Start here
+}
---
+++ src/challenge-2-ReactFunctionalComp.tsx
@@ -0,0 +1,38 @@
+function FunctionalComp() {
+  return (
+    <div id="functional-comp">
+      <h2>React Functional Component</h2>
+      Filter: <input placeholder="Filter by name" /> Multiplier:{" "}
+      <input
+        placeholder="Multiplier"
+        type="number"
+        min="1"
+        max="20"
+        defaultValue="10"
+      />{" "}
+      Press "Escape" to reset fields
+      <div className="loader">Loading...</div>
+      <table width="100%">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Height</th>
+            <th>Mass</th>
+            <th>Power</th>
+          </tr>
+        </thead>
+        <tbody>
+          {/* This is just an example, please remove */}
+          <tr>
+            <td>Example Character (please remove)</td>
+            <td>100</td>
+            <td>50</td>
+            <td>50000 (10 * 100 * 50)</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default FunctionalComp;
